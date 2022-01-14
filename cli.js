aba7d0b380dfc58aa4ed263b7c03dc012d320c7f
Nav Saini <navdeep@mailbox.org>
2022-01-14 13:50:09 +0530

    expand cli.js with screen selection logic

--- cli.js
+++ cli.js
@@ -4,6 +4,46 @@ require('@babel/register');
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const args = yargs(hideBin(process.argv)).argv
+const config = require('./config')
+const axios = require('axios')
+const ora = require('ora')
+const gpg = require('./util/gpg')
 
+const request = axios.create({
+  baseURL: config.baseURL,
+  timeout: 5000
+})
+
+if (args.i) {
+  const myGpgKey = gpg.myGpgKey()
+  const theirKey = gpg.getPublicKey(args.i)
+  request.post('/session', {
+      hostKey: myGpgKey,
+      guestKey: theirKey
+    }).then(() => {
+    })
+    .catch(e => {
+
+    })
+
+} else if (args.c) {
+  const guestKey = gpg.myGpgKey()
+  const hostKey = args.c
+  request.post('/connection', {
+    guestKey,
+    hostKey
+    }).then(() => {
+    })
+    .catch(e => {
+
+    })
+
+} else if (args.h) {
+  require('./components/Help')
+}
+else {
+  // list all gpg keys
+  require('./components/ParticipantSelect')
+}
 require('./index')
 // require('./components/App.jsx');
