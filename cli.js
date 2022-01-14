fcbf97d974c8c566701c86d4fb3e2e04cd2f2ac2
Nav Saini <navdeep@mailbox.org>
2022-01-15 00:11:02 +0530

    check existence of default key on startup

--- cli.js
+++ cli.js
@@ -1,49 +1,52 @@
 #!/usr/bin/env node
-require('@babel/register');
+require('@babel/register')
 
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const args = yargs(hideBin(process.argv)).argv
-const config = require('./config')
+const Conf = require('conf')
 const axios = require('axios')
 const ora = require('ora')
 const gpg = require('./util/gpg')
+const config = new Conf()
 
 const request = axios.create({
-  baseURL: config.baseURL,
+  baseURL: 'https://chatty.link',
   timeout: 5000
 })
 
-if (args.i) {
-  const myGpgKey = gpg.myGpgKey()
-  const theirKey = gpg.getPublicKey(args.i)
-  request.post('/session', {
-      hostKey: myGpgKey,
-      guestKey: theirKey
-    }).then(() => {
-    })
-    .catch(e => {
+if (config.get('myKeyId')) {
+  if (args.i) {
+    const myGpgKey = gpg.myGpgKey()
+    const theirKey = gpg.getPublicKey(args.i)
+    request.post('/session', {
+        hostKey: myGpgKey,
+        guestKey: theirKey
+      }).then(() => {
+      })
+      .catch(e => {
 
-    })
+      })
 
-} else if (args.c) {
-  const guestKey = gpg.myGpgKey()
-  const hostKey = args.c
-  request.post('/connection', {
-    guestKey,
-    hostKey
-    }).then(() => {
-    })
-    .catch(e => {
+  } else if (args.c) {
+    const guestKey = gpg.myGpgKey()
+    const hostKey = args.c
+    request.post('/connection', {
+      guestKey,
+      hostKey
+      }).then(() => {
+      })
+      .catch(e => {
 
-    })
+      })
 
-} else if (args.h) {
-  require('./components/Help')
+  } else if (args.h) {
+    require('./components/Help')
+  }
+  else {
+    // list all gpg keys
+    require('./components/ParticipantSelect')
+  }
+} else {
+// show key selection screen
 }
-else {
-  // list all gpg keys
-  require('./components/ParticipantSelect')
-}
-require('./index')
-// require('./components/App.jsx');
