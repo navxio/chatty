b96daf59432ac124ff6c21bef5e0cb196a397edd
Nav Saini <navdeep@mailbox.org>
2022-02-05 21:16:42 +0530

    formatting, add default key option

--- cli.js
+++ cli.js
@@ -1,72 +1,99 @@
 #!/usr/bin/env node
-require('@babel/register')
+require('@babel/register');
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const axios = require('axios')
-const ora = require('ora')
-const pkg = require('./package')
-const gpg = require('./gpg')
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const axios = require('axios');
+const ora = require('ora');
+const debug = require('debug');
+const gpg = require('./gpg');
+
+const { io } = require('socket.io-client');
+const socket = io('http://127.0.0.1:3000');
+
+socket.on('connect_error', (err) => {
+  console.error('connection error', err);
+});
+
+const spinner = ora('');
+const log = debug('user');
 
 const args = yargs(hideBin(process.argv))
-              .usage('Usage: chatty [options]')
-              .option('init', {
-                alias: 'i',
-                type: 'string',
-                description: 'Initiate a chat session'
-              })
-              .option('connect', {
-                alias: 'c',
-                type: 'string',
-                description: 'Connect to an existing session'
-              })
-              .help('h')
-              .alias('h', 'help')
-              .epilog('Copyright 2022 Navdeep Saini')
-              .argv
+  .usage('Usage: chatty [options]')
+  .option('init', {
+    alias: 'i',
+    type: 'string',
+    description: 'Initiate a chat session',
+  })
+  .option('connect', {
+    alias: 'c',
+    type: 'string',
+    description: 'Connect to an existing session',
+  })
+  .option('default-key', {
+    alias: 'k',
+    type: 'string',
+    description: 'default key identifier',
+  })
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Copyright 2022 Navdeep Saini').argv;
 
 const request = axios.create({
-  baseURL: 'https://chatty.link',
-  timeout: 5000
-})
+  baseURL: 'http://127.0.0.1:3000',
+  timeout: 5000,
+});
 
 /*
-* initiates a new session with a gpg key id
-  *
-*/
+ * initiates a new session with a gpg key id
+ *
+ */
 
 // flags -h, -c, -i are supposed to use in seclusion
 // using one negates the utility of another
 if (args.c) {
   // connect to an existing session
-  const spinner = ora(`connecting to an existing session with ${args.c}`)
-  const guestKey = gpg.myGpgKey()
-  const hostKey = args.c
-  request.post('/connection', {
-          guestKey,
-          hostKey
-  }).then(() => {
-      spinner.stop()
-  })
-  .catch(e => {
-      console.error(e)
-      process.exit(1)
-  })
+  spinner.text = `connecting to an existing session with ${args.c}`;
+  spinner.start();
+  const guestKey = gpg.myGpgKey();
+  const hostKey = args.c;
+  debug('joining a connection------------');
+  debug('key-> ' + hostKey + ':' + guestKey);
+  request
+    .post('/connection', {
+      guestKey,
+      hostKey,
+    })
+    .then((result) => {
+      log('result:' + JSON.stringify(result));
+      spinner.stop();
+    })
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    });
 } else if (args.i) {
-  const spinner = ora(`creating a new session with ${args.i}`).start()
-  const myGpgKey = gpg.myGpgKey()
-  const theirKey = gpg.getPublicKey(args.i)
-  request.post('/session', {
-    hostKey: myGpgKey,
-    guestKey: theirKey
-  }).then(() => {
-      spinner.stop()
-  })
-  .catch(e => {
-      console.error(e)
-      process.exit(1)
-  })
+  spinner.text = `creating a new session with ${args.i}`;
+  spinner.start();
+  const hostKey = gpg.myGpgKey();
+  const guestKey = gpg.getPublicKey(args.i);
+  debug('creating a connection------------');
+  debug('key-> ' + hostKey + ':' + guestKey);
+  request
+    .post('/session', {
+      hostKey,
+      guestKey,
+    })
+    .then((result) => {
+      log('result data:' + JSON.stringify(result.data));
+      spinner.stop();
+      socket.emit('verify connection', { roomId: result.data.id });
+    })
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    });
 } else {
   // no flag was passed - render a select screen carrying all gpg keys available for chat
-  require('./components/ParticipantSelect')
+  require('./components/ParticipantSelect');
 }
