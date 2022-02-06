5438d4e981b06fab95adaf54e0779e9a7431812a
Nav Saini <navdeep@mailbox.org>
2022-02-06 19:50:41 +0530

    init participant select screen with class component

---
+++ components/ParticipantSelect.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+class ParticipantSelect extends React.Component {
+  constructor() {}
+  render() {
+    return <box>hello</box>;
+  }
+}
+export default ParticipantSelect;
